20a6856914f9031bc2136d08f03270aafb77135b
Dobby_Kim <kimdobby@g.skku.edu>
2023-09-23 11:23:20 +0000

    승패 이외의 전적 내용 적용

--- 0923_base/services/GameService.js
+++ 0923_base/services/GameService.js
@@ -1,23 +1,27 @@
 import SummonerService from "./SummonerService.js";
 import MatchService from "./MatchService.js";
-import * as Promise from "bluebird";
+import Bluebird from "bluebird";
 export default class GameService {
+  static getLast20Games = async (summonerName) => {
+    try {
+      const pUuid = await SummonerService.getSummonerPuuid(summonerName);
+      console.log("pUuid Check: ", pUuid);
+      const matchIds = await MatchService.getMatchIdsByPUuid(pUuid);
+      console.log("matchIds Check: ", matchIds);
 
+      const tmp_ = await Bluebird.map(
+        matchIds,
+        async (matchId) => {
+          return await MatchService.getMatchResultByMatchId(matchId, pUuid);
+        },
+        { concurrency: 1 }
+      );
+      console.log("matchIds info: ", tmp_);
 
-    static  getLast20Games = async (summonerName) => {
-        try {
-
-            const pUuid = await SummonerService.getSummonerPuuid(summonerName)
-            const matchIds = await MatchService.getMatchIdsByPUuid(pUuid);
-
-            return Promise.mapSeries(matchIds,async (matchId) => {
-                return await MatchService.getMatchResultByMatchId(matchId, pUuid) ? "승리" : "패배;"
-            })
-
-        } catch (error) {
-            console.error("Error fetching data:", error);
-            throw error;
-        }
-    };
-
+      return tmp_;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      throw error;
+    }
+  };
 }
--- 0923_base/services/MatchService.js
+++ 0923_base/services/MatchService.js
@@ -1,26 +1,43 @@
 import axios from "axios";
-import {BASE_URL_MATCH, HEADERS} from "../config/index.js";
+import { BASE_URL_MATCH, HEADERS } from "../config/index.js";
 
 export default class MatchService {
+  static apiClient = axios.create({
+    headers: HEADERS,
+  });
 
-    static apiClient = axios.create({
-        headers: HEADERS,
-    });
+  static getMatchIdsByPUuid = async (pUuid) => {
+    const response = await this.apiClient.get(
+      `${BASE_URL_MATCH}/by-puuid/${pUuid}/ids?start=0&count=20`
+    );
+    return response.data;
+  };
 
-    static getMatchIdsByPUuid = async (pUuid) => {
-        const response = await this.apiClient.get(
-            `${BASE_URL_MATCH}/by-puuid/${pUuid}/ids?start=0&count=20`
-        );
-        return response.data;
-    };
+  static getMatchResultByMatchId = async (matchId, pUuid) => {
+    try {
+      const response = await this.apiClient.get(`${BASE_URL_MATCH}/${matchId}`);
+      const participant = response.data.info.participants.find(
+        (p) => p.puuid === pUuid
+      );
 
-    static getMatchResultByMatchId = async (matchId, pUuid) => {
-        const response = await this.apiClient.get(`${BASE_URL_MATCH}/${matchId}`);
-        const participant = response.data.info.participants.find(
-            (p) => p.puuid === pUuid
-        );
-        return participant.win;
-    };
+      if (!participant) {
+        console.error("Participant not found:", pUuid);
+        return null;
+      }
 
+      const matchResult = {
+        win: participant.win,
+        championName: participant.championName,
+        kills: participant.kills,
+        deaths: participant.deaths,
+        assists: participant.assists,
+        largestMultiKill: participant.largestMultiKill,
+      };
 
+      return matchResult;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      return null;
+    }
+  };
 }
